8b10f5333ae98baa32fb75e4d62e8522ab05b954
peas8810 <127704090+peas8810@users.noreply.github.com>
2025-04-19 15:29:45 -0300

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,14 @@
+import datetime
+import pandas as pd
+from fpdf import FPDF
+from docx import Document
+from bs4 import BeautifulSoup
 
+# Aqui vão todas as funções puras de tratamento de dados:
+# - converter_data
+# - calcular_status_processo
+# - exportar_pdf, exportar_docx, gerar_relatorio_pdf
+# - aplicar_filtros, get_dataframe_with_cols
+# - consultar_movimentacoes_simples
+
+# (Copie e cole nelas o conteúdo de utils.py atualmente)
